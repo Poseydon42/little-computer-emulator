116e5308c70140bbd52bcc422af8f969e4eed16e
Poseydon42 <vvmposeydon@gmail.com>
2022-12-07 14:41:40 +0000

    Added enums and structs for opcodes, registers and instructions

---
+++ assembler/include/Instruction.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <cstdint>
+#include <variant>
+
+namespace lce::Assembler
+{
+    enum class Opcode
+    {
+        Mov,
+        Add,
+        Sub,
+        And,
+        Or,
+        Xor,
+        Not,
+        Push,
+        Pop,
+        Jmp,
+        Jz,
+        Jg,
+        Call,
+        Ret,
+        Hlt
+    };
+
+    enum class OperandType
+    {
+        None,
+
+        Immediate,
+        Register,
+        ImmediateAddress,
+        RegisterAddress
+    };
+
+    enum class Register : uint8_t
+    {
+        R0 = 0b000,
+        R1 = 0b001,
+        R2 = 0b010,
+        R3 = 0b011,
+
+        RSP = 0b100,
+        RFL = 0b101
+    };
+
+    struct Operand
+    {
+        OperandType Type = OperandType::None;
+        std::variant<uint64_t, Register> Value;
+    };
+
+    struct Instruction
+    {
+        Opcode Opcode = Opcode::Hlt;
+
+        Operand Operands[2];
+    };
+} // namespace lce::Assembler
