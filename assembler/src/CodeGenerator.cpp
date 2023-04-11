61de92300f24c3cc24d250701dddb2b47f0e3a3b
Poseydon42 <vvmposeydon@gmail.com>
2023-04-11 18:28:38 +0100

    Implemented code generation for every possible valid combination of operands

--- assembler/src/CodeGenerator.cpp
+++ assembler/src/CodeGenerator.cpp
@@ -1,54 +1,158 @@
 #include "CodeGenerator.h"
 
 #include <cassert>
+#include <cstdint>
+#include <limits>
 #include <unordered_map>
 
 #include "ErrorReporting.h"
-
-#define FIXME()                                                                                            \
-    {                                                                                                      \
-        Common::ReportError(Common::ErrorSeverity::Fatal, {}, "FIXME at {}(line {})", __FILE__, __LINE__); \
-        assert(false);                                                                                     \
-    }
+#include "Instruction.h"
 
 namespace lce::Assembler
 {
-    static void MovGenerator(const Instruction& Instruction, std::vector<uint8_t>& Destination)
+    static constexpr auto OpcodeLength = 6;
+    static constexpr auto OpcodeShift = 8 - OpcodeLength;
+
+    static constexpr auto FirstOperandTypeShift = 0;
+    static constexpr auto SecondOperandTypeShift = 6;
+
+    static constexpr auto FirstRegisterOperandShift = 3;
+    static constexpr auto SecondRegisterOperandShift = 0;
+
+    static constexpr uint8_t OperandTypeRegister = 0b00;
+    static constexpr uint8_t OperandTypeOneByteImmediate = 0b10;
+    static constexpr uint8_t OperandTypeTwoByteImmediate = 0b11;
+
+    static std::pair<uint8_t, uint8_t> ExtractImmediate(const Instruction& Instruction, size_t OperandIndex)
     {
-        if (Instruction.Operands[0].Type == OperandType::Register && Instruction.Operands[1].Type == OperandType::Immediate)
-        {
-            static constexpr uint8_t OpcodeMask = 0b00010000;
+        assert(OperandIndex < 2);
 
-            uint8_t OpcodeByte = OpcodeMask | static_cast<uint8_t>(std::get<Register>(Instruction.Operands[0].Value));
-            
-            uint8_t LowImmediateByte = static_cast<uint8_t>(std::get<uint64_t>(Instruction.Operands[1].Value) & 0xFF);
-            uint8_t HighImmediateByte = static_cast<uint8_t>((std::get<uint64_t>(Instruction.Operands[1].Value) >> 8) & 0xFF);
+        auto RawValue = std::get<uint64_t>(Instruction.Operands[OperandIndex].Value);
+        if (RawValue > std::numeric_limits<uint16_t>::max())
+            Common::ReportError(Common::ErrorSeverity::Warning, Instruction.Location, "Immediate argument value {} exceeds 16 bits; a truncated version will be written", RawValue);
 
-            Destination.push_back(OpcodeByte);
-            Destination.push_back(LowImmediateByte);
-            Destination.push_back(HighImmediateByte);
-        }
-        else
-        {
-            FIXME();
-        }
+        auto MostSignificanByte = static_cast<uint8_t>(RawValue >> 8);
+        auto LeastSignificantByte = static_cast<uint8_t>(RawValue >> 0);
+
+        return std::make_pair(MostSignificanByte, LeastSignificantByte);
     }
 
     static void GenerateMachineCodeForInstruction(const Instruction& Instruction, std::vector<uint8_t>& Destination)
     {
-        using InstructionGeneratorFuncType = void (*)(const struct Instruction&, std::vector<uint8_t>&);
-        static const std::unordered_map<Opcode, InstructionGeneratorFuncType> InstructionGenerators = {
-            { Opcode::Mov, MovGenerator }
-        };
+        uint8_t Opcode = static_cast<uint8_t>(Instruction.Opcode);
 
-        if (!InstructionGenerators.contains(Instruction.Opcode))
+        if (Instruction.Operands[0].Type == OperandType::None && Instruction.Operands[1].Type == OperandType::None)
         {
-            Common::ReportError(Common::ErrorSeverity::Fatal, {}, "Unimplemented generator for opcode");
+            uint8_t FirstByte = Opcode << OpcodeShift;
+            Destination.push_back(FirstByte);
+        }
+        else if (Instruction.Operands[0].Type == OperandType::Register && Instruction.Operands[1].Type == OperandType::None)
+        {
+            auto RegisterIndex = static_cast<uint8_t>(std::get<Register>(Instruction.Operands[0].Value));
+
+            uint8_t FirstByte = (Opcode << OpcodeShift) | (OperandTypeRegister << FirstOperandTypeShift);
+            uint8_t SecondByte = RegisterIndex << FirstRegisterOperandShift;
+
+            Destination.push_back(FirstByte);
+            Destination.push_back(SecondByte);
+        }
+        else if (Instruction.Operands[0].Type == OperandType::Immediate && Instruction.Operands[1].Type == OperandType::None)
+        {
+            auto [MSB, LSB] = ExtractImmediate(Instruction, 0);
+            
+            uint8_t FirstOperandTypeBitcode;
+            if (MSB > 0)
+                FirstOperandTypeBitcode = OperandTypeTwoByteImmediate;
+            else
+                FirstOperandTypeBitcode = OperandTypeOneByteImmediate;
+
+            uint8_t FirstByte = (Opcode << OpcodeShift) | (FirstOperandTypeBitcode << FirstOperandTypeShift);
+            Destination.push_back(FirstByte);
+
+            if (FirstOperandTypeBitcode == OperandTypeOneByteImmediate)
+            {
+                uint8_t SecondByte = static_cast<uint8_t>(LSB);
+                Destination.push_back(SecondByte);
+            }
+            else
+            {
+                uint8_t SecondByte = static_cast<uint8_t>(LSB);
+                uint8_t ThirdByte = static_cast<uint8_t>(MSB);
+                Destination.push_back(SecondByte);
+                Destination.push_back(ThirdByte);
+            }
+        }
+        else if (Instruction.Operands[0].Type == OperandType::Register && Instruction.Operands[1].Type == OperandType::Register)
+        {
+            auto FirstRegister = std::get<Register>(Instruction.Operands[0].Value);
+            auto SecondRegister = std::get<Register>(Instruction.Operands[1].Value);
+
+            auto FirstRegisterBits = static_cast<uint8_t>(FirstRegister);
+            auto SecondRegisterBits = static_cast<uint8_t>(SecondRegister);
+
+            uint8_t FirstByte = (Opcode << OpcodeShift) | (OperandTypeRegister << FirstOperandTypeShift);
+            uint8_t SecondByte = (OperandTypeRegister << SecondOperandTypeShift) | (FirstRegisterBits << FirstRegisterOperandShift) | (SecondRegisterBits << SecondRegisterOperandShift);
+
+            Destination.push_back(FirstByte);
+            Destination.push_back(SecondByte);
+        }
+        else if (Instruction.Operands[0].Type == OperandType::Register && Instruction.Operands[1].Type == OperandType::Immediate)
+        {
+            auto RegisterBits = static_cast<uint8_t>(std::get<Register>(Instruction.Operands[0].Value));
+
+            uint8_t FirstByte = (Opcode << OpcodeShift) | (OperandTypeRegister << FirstOperandTypeShift);
+            Destination.push_back(FirstByte);
+
+            auto [MSB, LSB] = ExtractImmediate(Instruction, 1);
+            if (MSB > 0)
+            {
+                uint8_t SecondByte = (OperandTypeTwoByteImmediate << SecondOperandTypeShift) | (RegisterBits << FirstRegisterOperandShift);
+                uint8_t ThirdByte = LSB;
+                uint8_t FourthByte = MSB;
+
+                Destination.push_back(SecondByte);
+                Destination.push_back(ThirdByte);
+                Destination.push_back(FourthByte);
+            }
+            else
+            {
+                uint8_t SecondByte = (OperandTypeOneByteImmediate << SecondOperandTypeShift) | (RegisterBits << FirstRegisterOperandShift);
+                uint8_t ThirdByte = LSB;
+
+                Destination.push_back(SecondByte);
+                Destination.push_back(ThirdByte);
+            }
+        }
+        else if (Instruction.Operands[0].Type == OperandType::Immediate && Instruction.Operands[1].Type == OperandType::Register)
+        {
+            auto [MSB, LSB] = ExtractImmediate(Instruction, 0);
+            auto RegisterBits = static_cast<uint8_t>(std::get<Register>(Instruction.Operands[1].Value));
+
+            if (MSB > 0)
+            {
+                uint8_t FirstByte = (Opcode << OpcodeShift) | (OperandTypeTwoByteImmediate << FirstOperandTypeShift);
+                uint8_t SecondByte = (OperandTypeRegister << SecondOperandTypeShift) | (RegisterBits << SecondRegisterOperandShift);
+
+                Destination.push_back(FirstByte);
+                Destination.push_back(SecondByte);
+                Destination.push_back(LSB);
+                Destination.push_back(MSB);
+            }
+            else
+            {
+                uint8_t FirstByte = (Opcode << OpcodeShift) | (OperandTypeOneByteImmediate << FirstOperandTypeShift);
+                uint8_t SecondByte = (OperandTypeRegister << SecondOperandTypeShift) | (RegisterBits << SecondRegisterOperandShift);
+
+                Destination.push_back(FirstByte);
+                Destination.push_back(SecondByte);
+                Destination.push_back(LSB);
+            }
+        }
+        else
+        {
+            // NOTE: any other combination is invalid
             assert(false);
         }
-
-        auto Generator = InstructionGenerators.at(Instruction.Opcode);
-        Generator(Instruction, Destination);
     }
 
     std::vector<uint8_t> GenerateMachineCode(const std::vector<Instruction>& Insturctions)
--- assembler/tests/TestCodeGenerator.cpp
+++ assembler/tests/TestCodeGenerator.cpp
@@ -4,16 +4,138 @@
 #include "CodeGenerator.h"
 #include "Instruction.h"
 
-TEST(TestCodeGenerator, GenerateSimpleMov)
+TEST(TestCodeGenerator, NoOperands)
 {
-    lce::Assembler::Instruction Mov = {};
-    Mov.Opcode = lce::Assembler::Opcode::Mov;
-    Mov.Operands[0] = { lce::Assembler::OperandType::Register, lce::Assembler::Register::R0 };
-    Mov.Operands[1] = { lce::Assembler::OperandType::Immediate, 1337u };
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Nop;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::None;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::None;
 
-    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Mov });
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
+    ASSERT_EQ(GeneratedBytes.size(), 1);
+    EXPECT_EQ(GeneratedBytes[0], 0b01010100);
+}
+
+TEST(TestCodeGenerator, OneRegisterOperand)
+{
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Pop;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::Register;
+    Instruction.Operands[0].Value = lce::Assembler::Register::R1;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::None;
+
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
+    ASSERT_EQ(GeneratedBytes.size(), 2);
+    EXPECT_EQ(GeneratedBytes[0], 0b00110100);
+    EXPECT_EQ(GeneratedBytes[1], 0b00001000);
+}
+
+TEST(TestCodeGenerator, OneSingleByteImmediateOperand)
+{
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Push;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::Immediate;
+    Instruction.Operands[0].Value = 42ull;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::None;
+
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
+    ASSERT_EQ(GeneratedBytes.size(), 2);
+    EXPECT_EQ(GeneratedBytes[0], 0b00110010);
+    EXPECT_EQ(GeneratedBytes[1], 42);
+}
+
+TEST(TestCodeGenerator, OneDoubleByteImmediateOperand)
+{
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Push;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::Immediate;
+    Instruction.Operands[0].Value = 420ull;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::None;
+
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
     ASSERT_EQ(GeneratedBytes.size(), 3);
-    EXPECT_EQ(GeneratedBytes[0], 0b00010000);
-    EXPECT_EQ(GeneratedBytes[1], 0b00111001);
-    EXPECT_EQ(GeneratedBytes[2], 0b00000101);
+    EXPECT_EQ(GeneratedBytes[0], 0b00110011);
+    EXPECT_EQ(GeneratedBytes[1], 420 % 256);
+    EXPECT_EQ(GeneratedBytes[2], 420 >> 8);
+}
+
+TEST(TestCodeGenerator, TwoRegisterOperands)
+{
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Mov;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::Register;
+    Instruction.Operands[0].Value = lce::Assembler::Register::R2;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::Register;
+    Instruction.Operands[1].Value = lce::Assembler::Register::R3;
+
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
+    ASSERT_EQ(GeneratedBytes.size(), 2);
+    EXPECT_EQ(GeneratedBytes[0], 0b00000100);
+    EXPECT_EQ(GeneratedBytes[1], 0b00010011);
+}
+
+TEST(TestCodeGenerator, FirstRegisterSecondSingleByteImmediate)
+{
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Lda;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::Register;
+    Instruction.Operands[0].Value = lce::Assembler::Register::R2;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::Immediate;
+    Instruction.Operands[1].Value = 42ull;
+
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
+    ASSERT_EQ(GeneratedBytes.size(), 3);
+    EXPECT_EQ(GeneratedBytes[0], 0b00001000);
+    EXPECT_EQ(GeneratedBytes[1], 0b10010000);
+    EXPECT_EQ(GeneratedBytes[2], 42);
+}
+
+TEST(TestCodeGenerator, FirstRegisterSecondDoubleByteImmediate)
+{
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Lda;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::Register;
+    Instruction.Operands[0].Value = lce::Assembler::Register::R2;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::Immediate;
+    Instruction.Operands[1].Value = 420ull;
+
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
+    ASSERT_EQ(GeneratedBytes.size(), 4);
+    EXPECT_EQ(GeneratedBytes[0], 0b00001000);
+    EXPECT_EQ(GeneratedBytes[1], 0b11010000);
+    EXPECT_EQ(GeneratedBytes[2], 420 % 256);
+    EXPECT_EQ(GeneratedBytes[3], 420 >> 8);
+}
+
+TEST(TestCodeGenerator, FirstSingleByteImmediateSecondRegister)
+{
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Sta;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::Immediate;
+    Instruction.Operands[0].Value = 42ull;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::Register;
+    Instruction.Operands[1].Value = lce::Assembler::Register::R3;
+
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
+    ASSERT_EQ(GeneratedBytes.size(), 3);
+    EXPECT_EQ(GeneratedBytes[0], 0b00001110);
+    EXPECT_EQ(GeneratedBytes[1], 0b00000011);
+    EXPECT_EQ(GeneratedBytes[2], 42);
+}
+
+TEST(TestCodeGenerator, FirstDoubleByteImmediateSecondRegister)
+{
+    lce::Assembler::Instruction Instruction = {};
+    Instruction.Opcode = lce::Assembler::Opcode::Sta;
+    Instruction.Operands[0].Type = lce::Assembler::OperandType::Immediate;
+    Instruction.Operands[0].Value = 420ull;
+    Instruction.Operands[1].Type = lce::Assembler::OperandType::Register;
+    Instruction.Operands[1].Value = lce::Assembler::Register::R3;
+
+    auto GeneratedBytes = lce::Assembler::GenerateMachineCode({ Instruction });
+    ASSERT_EQ(GeneratedBytes.size(), 4);
+    EXPECT_EQ(GeneratedBytes[0], 0b00001111);
+    EXPECT_EQ(GeneratedBytes[1], 0b00000011);
+    EXPECT_EQ(GeneratedBytes[2], 420 % 256);
+    EXPECT_EQ(GeneratedBytes[3], 420 >> 8);
 }
